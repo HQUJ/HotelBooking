ee9af96b868d729a144a449f5817bcf757d4f1be
HQUJ <hristina.mecheva@gmail.com>
2024-04-11 11:39:00 +0300

    Промени 11/4

--- Models/Booking2.cs
+++ Models/Booking2.cs
@@ -13,17 +13,26 @@ namespace Hotel9.Models
 
         [ScaffoldColumn(false)]
         public string? ClientUsername { get; set; }
+
         [ScaffoldColumn(false)]
         public int? RoomId { get; set; }
+
         [Required]
         public string RoomType { get; set; }
         
         [Required]
+        [DateGreaterThanToday]
         public DateTime CheckIn { get; set; }
+
         [Required]
+        [Display(Name = "Stay Duration")]
+        [Range(1, 20, ErrorMessage = "Stay Duration must be between 1 and 20 days")]
         public int StayDuration { get; set; }
 
         public Booking2() { }
+
+        
+        //Създава Booking2 по модел на друг такъв обект
         public Booking2(Object b)
         {
             if(b is Booking2)
